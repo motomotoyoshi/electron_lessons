ca941aa66ff15da8a4dc4106faaa10b38c47dbf8
motomotoyoshi <motomoto.git@gmail.com>
2020-06-06 01:44:08 +0900

    update, index.js

--- MyApp/index.js
+++ MyApp/index.js
@@ -9,12 +9,28 @@ const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-app.on('ready', function() {
-  // create window
-  mainWindow = new BrowserWindow({width: 600, height: 400});
+function createMainWindow() {
+  mainWindow = new BrowserWindow({ width: 600, height: 400 });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', function() {
+  mainWindow.on("closed", function () {
     mainWindow = null;
-  })
+  });
+}
+
+app.on('ready', function() {
+  // create window
+  createMainWindow();
+});
+
+app.on('window-all-closed', function() {
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if(mainWindow === null) {
+    createMainWindow();
+  }
 });
