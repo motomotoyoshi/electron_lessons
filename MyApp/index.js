e07a89caa57db3f354abbdb424b5f4c693451f42
motomotoyoshi <motomoto.git@gmail.com>
2020-06-06 17:21:02 +0900

    Add, menu

--- MyApp/index.js
+++ MyApp/index.js
@@ -6,10 +6,25 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow;
 
+let menuTemplate = [{
+  label: 'MyApp',
+  submenu: [
+    { label: 'About'},
+    { type: 'separator'},
+    { label: 'Settings'},
+    { type: 'separator' },
+    { label: 'Quit' },
+  ]
+}];
+
+let menu = Menu.buildFromTemplate(menuTemplate);
+
 function createMainWindow() {
+  Menu.setApplicationMenu(menu);
   mainWindow = new BrowserWindow({ width: 600, height: 400 });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.webContents.openDevTools();
